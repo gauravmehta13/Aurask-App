6cf25ce481085396ee05e99b9f68f023ec4cf2c3
Gaurav <270mehta@gmail.com>
2021-08-21 09:41:43 +0530

    minor, Solved Okhttp3 error

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
-import 'dart:io';
-
 import 'package:aurask/core/resources/login_Provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_redux/flutter_redux.dart';
